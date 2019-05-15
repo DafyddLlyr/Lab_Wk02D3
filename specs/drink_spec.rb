11498c7945f8ab2c5e7cbe154cbcf11e808b1f24
dafyddllyr <dafyddllyr@gmail.com>
2019-05-15 10:18:58 +0100

    Drink class compelted

---
+++ specs/drink_spec.rb
@@ -0,0 +1,19 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../drink")
+
+class DrinkTest < MiniTest::Test
+
+  def setup
+    @drink = Drink.new("Irn Bru", 2)
+  end
+
+  def test_drink_name
+    assert_equal("Irn Bru", @drink.name)
+  end
+
+  def test_drink_price
+    assert_equal(2, @drink.price)
+  end
+
+end
